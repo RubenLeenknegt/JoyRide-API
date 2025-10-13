49bfcb11d59f46ef54d93e60c9d54dc560d69082
gielvangaal <gielvangaal@gmail.com>
2025-10-13 20:20:32 +0200

    Implemented HTTP test and more

--- backend/src/main/kotlin/leafcar/backend/service/CarService.kt
+++ backend/src/main/kotlin/leafcar/backend/service/CarService.kt
@@ -1,17 +1,19 @@
 package leafcar.backend.service
 
 import leafcar.backend.dao.CarEntity
+import leafcar.backend.domain.Car
 import leafcar.backend.dto.request.CarCreateRequest
 import leafcar.backend.mappers.CarMapper
+import leafcar.backend.mappers.CarMapper.toDomain
 import leafcar.backend.repository.CarRepository
 
 class CarService(
     private val carRepository: CarRepository
 ) {
 
-    fun createCar(request: CarCreateRequest, ownerId: String): CarEntity {
-        val car = CarMapper.toCarCreateRequest(request, ownerId)
-        // eventueel extra validaties of rules hier
-        return carRepository.create(car)
+    fun createCar(request: CarCreateRequest, ownerId: String): Car {
+        val carEntity = CarMapper.toCarCreateRequest(request, ownerId)
+        val savedEntity = carRepository.create(carEntity)
+        return savedEntity.toDomain()
     }
 }
--- backend/src/test/kotlin/CarControllerTest.kt
+++
@@ -1,8 +0,0 @@
-//class CarControllerTest {
-//    fun `GET users returns seeded users`() {
-//        application ( module() )
-//        val repsonse = client.get("/cars")
-//        assertEquals(HttpResponseStatus.OK, response.status())
-//
-//    }
-//}
