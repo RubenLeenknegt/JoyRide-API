aae0ea1ccae0382ec82a6fdfa5ac4c5bc54a6c0e
ivarv <ivarvisser@hotmail.com>
2025-11-16 15:01:21 +0100

    JOY-140 rest of the tables were fine. some finetuning and the actuall setup function created

--- backend/src/main/kotlin/joyride/backend/services/DatabaseSetup.kt
+++ backend/src/main/kotlin/joyride/backend/services/DatabaseSetup.kt
@@ -1,17 +1,22 @@
-//package joyride.backend.services
-//
-//import org.jetbrains.exposed.sql.SchemaUtils
-//
-//object DatabaseSetup {
-//    transaction(database) {
-//        SchemaUtils.create(
-//            Users,
-//            CarsTable,
-//            Reservations,
-//            AvailabilitiesTable,
-//            Rides,
-//            Photos,
-//            BonusPointsTable
-//        )
-//    }
-//}
+package joyride.backend.services
+
+import org.jetbrains.exposed.sql.SchemaUtils
+import joyride.backend.dao.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object DatabaseSetup {
+    fun createTables(database: Database) {
+        transaction(database) {
+            SchemaUtils.create(
+                UsersTable,
+                CarsTable,
+                ReservationsTable,
+                AvailabilitiesTable,
+                RidesTable,
+                PhotosTable,
+                BonusPointsTable
+            )
+        }
+    }
+}
